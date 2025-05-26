b962682515d216739efbfa4ff0898344bfdaff94
guzio <star.master.123.mg@gmail.com>
2025-05-26 06:45:35 +0200

    welcome message

--- projekt.php
+++ projekt.php
@@ -8,6 +8,9 @@
         <hr>
             <h1> PHANTOM </h1>
         </hr>
+        Witamy w systemie PHANTOM! <br>
+        Tutaj miała znajdować się strona logowania, ale z powodu „cięć budżetowych” nie udało nam się tego zaimplementować. Ponieważ nie ma żadnego systemu autoryzacji dostępu, strona została ograniczona do statycznego wyświetlania tabel z naszej bazy danych, żeby żaden griefer nie przyszedł i nie popsuł. <br>
+        Życzymy miłego pobytu! <br>
         <?php
             // tu umieszczamy kod skryptu
             // Połączmy się z bazą danych i rozłączmy się z nią
